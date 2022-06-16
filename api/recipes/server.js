2a480533dbec6426586e70f3db79102a1357df6c
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-16 18:40:17 -0500

    wiring complete

--- api/recipes/server.js
+++ api/recipes/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+
 const recipesRouter = require('./recipes-router')
+
 const server = express()
 
 server.use(express.json())
 
+
 server.use('/api/recipes', recipesRouter)
+
+server.use('*', (req, res, next) => {
+    res.json({ api: 'up' })
+})
 
 module.exports = server
