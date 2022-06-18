70b6ade457e3c2ff9eabf0a32da2113f2331fae9
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-18 14:20:54 -0500

    seeds initiated

--- data/seeds/02-steps.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = function(knex, Promise) {
-return knex('steps').insert([
-    {recipe_name: "Cereal", step_number: 1, step_instructions: "Pour cereal in bowl"},
-    {recipe_name: "Cereal", step_number: 2, step_instructions: "Add milk to bowl"},
-    {recipe_name: "butter sandwhich", step_number: 1, step_instructions: "get bread"},
-    {recipe_name: "butter sandwhich", step_number: 2, step_instructions: "butter bread"},
-])
-}
